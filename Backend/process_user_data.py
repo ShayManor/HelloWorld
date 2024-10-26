e72c02a9ef01fc88b1167be4a9f14f5e688fb023
shay <shay@sentra.io>
2024-10-26 09:31:21 -0400

    Added class to process user input

---
+++ Backend/process_user_data.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+
+class process_data:
+    def __init__(self, problem, solution):
+        OpenAI.api_key = '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'
+        self.assistant_id = "asst_VRHhu2ImFPxGBi05iz2AnjOc"
+        self.client = OpenAI()
+        self.prompt = "Problem: " + problem + "\n Solution: " + solution
+
+    def start_process(self):
+        assistant = self.client.beta.assistants.retrieve(
+            assistant_id=self.assistant_id
+        )
+        thread = self.client.beta.threads.create(
+            messages=[{"role": "user", "content": self.problem}]
+        )
+
+        run = self.client.beta.threads.runs.create_and_poll(
+            thread_id=thread.id,
+            assistant_id=assistant.id,
+        )
+
+        if run.status == 'completed':
+            messages = self.client.beta.threads.messages.list(thread_id=run.thread_id)
+            ai_response = messages.data[0].content[0].text.value
+            if ai_response == "false":
+                return "Error"
+            return ai_response
+        else:
+            return "Error"
+
